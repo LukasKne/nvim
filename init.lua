f43bd4a552e86cac4928e0183aff71187fe91794
Khoa Nguyen <dakennguyen@gmail.com>
2025-10-26 23:23:47 +0100

    Use exrc

--- init.lua
+++ init.lua
@@ -8,5 +8,3 @@ require "config.autocmds"
 require "plugins"
 
 vim.notify(vim.fn.printf("Loaded in %.0fms", vim.fn.reltimefloat(vim.fn.reltime(start_time)) * 1000))
-
-if vim.fn.filereadable ".git/safe/../../.nvimrc" == 1 then vim.cmd "source .nvimrc" end
